d2f2da9e568a8ef9afb5017d5722c838229612a4
Mol saphat <m.sophat27@gmail.com>
2021-11-20 14:34:22 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
