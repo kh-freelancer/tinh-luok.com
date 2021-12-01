52ec7309b3539ff90e589e64e0fe8c5034664e96
Mol saphat <m.sophat27@gmail.com>
2021-12-02 04:54:35 +0700

    update

--- pages/products/[product_detail].js
+++ pages/products/[product_detail].js
@@ -63,9 +63,9 @@ export default function ProductDetail(){
                                         <button className="w-1/2 flex items-center justify-center rounded-md bg-primary hover:bg-secondaray text-white" type="submit">Buy now</button>
                                         <button className="w-1/2 flex items-center justify-center rounded-md border border-gray-300" type="button">Add to bag</button>
                                     </div>
-                                    <button claclassNamess="flex-none flex items-center justify-center w-9 h-9 rounded-md text-gray-400 border border-gray-300" type="button" aria-label="like">
+                                    <button className="flex-none flex items-center justify-center w-9 h-9 rounded-md text-gray-400 border border-gray-300" type="button" aria-label="like">
                                         <svg width="20" height="20" fill="currentColor">
-                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+                                        <path fillRule="evenodd" clipRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
                                         </svg>
                                     </button>
                                 </div>
